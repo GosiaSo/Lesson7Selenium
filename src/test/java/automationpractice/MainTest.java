9c8350ecd94e27d1864dfc438a24cbade972b543
msobocinska <msobocinska@sii.pl>
2022-08-28 11:38:40 +0200

    config

--- src/test/java/automationpractice/MainTest.java
+++ src/test/java/automationpractice/MainTest.java
@@ -1,6 +1,10 @@
-public class MainTest extends WebDriverManagerConfig{
+package automationpractice;
 
+import config.WebDriverManagerConfig;
 
+public class MainTest extends WebDriverManagerConfig {
+
+    private static final String AUTOMATION_PRACTICE = "http://automationpractice.com/index.php";
 
 
 }
